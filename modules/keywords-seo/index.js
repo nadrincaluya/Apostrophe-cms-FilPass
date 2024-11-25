b6000fc7a333c748618e3ad90276e786e5d1b93b
nadrincaluya <nadrincaluya@gmail.com>
2024-11-25 22:38:10 +0800

    Minor Changes

---
+++ modules/keywords-seo/index.js
@@ -0,0 +1,12 @@
+module.exports = {
+    improve: '@apostrophecms/seo',
+    fields: {
+      add: {
+        keywords: {
+          type: 'string',
+          label: 'Keywords',
+          help: 'Comma-separated list of keywords for SEO'
+        }
+      }
+    }
+  };
